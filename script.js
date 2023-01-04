e8971a691a2e2d0657ae23a7e840d96eb30e455f
Steven Nguyen <omgsteven@gmail.com>
2023-01-03 20:59:29 -0500

    modified logic

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
 
-
-// Generates target number
-function generateTarget(){
-   return Math.floor(Math.random()*10);
+const generateTarget = () =>  {
+    return Math.floor(Math.random() * 10);
 }
+// Gives a target number between 0 and 9
 
-
-// Compares the human choice vs computer
-function compareGuesses(userChoice,computerChoice,target){
-  const compHuman = Math.abs(target - userChoice);
-  const compComputer = Math.abs(target-computerChoice);
-
-  return compHuman <= compComputer;
-  }
-
-  //Updates score
-function updateScore(winner){
-  if(winner === 'human'){
-    humanScore ++;
-  }
-  else if(winner ==='computer'){
-    computerScore ++;
-  }
+const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
+    if (Math.abs(humanGuess - targetGuess) > Math.abs(computerGuess - targetGuess)) {
+        return true;
+    };
+    
+    if (Math.abs(humanGuess - targetGuess) < Math.abs (computerGuess - targetGuess)) {
+        return false;
+    };
 }
-//Creates new round
-function advanceRound(){
-  currentRoundNumber ++;
-}
+// Determines winner based on closest guess to target
+
+const updateScore= (winner) => {
+    if (winner === 'human') {
+        humanScore ++;
+    } else if (winner === 'computer') {
+        computerScore ++;
+    } 
+};
+// Updates score after win
+
+const advanceRound = () => currentRoundNumber ++;
+// Updates the round number after each round.
+
+updateScore('human');
+console.log(humanScore); 
+// To confirm that this value increased by 1
+
+updateScore('computer');
+console.log(computerScore); // To confirm that this value increased by 1
